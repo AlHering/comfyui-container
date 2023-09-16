ccf8970e7e379962caab71cfcedd9676d695dedc
Alexander Hering <dev.hering@gmail.com>
2023-09-16 15:37:49 +0200

    added installation script

---
+++ install_webui.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /comfyui-container/venv/bin/activate
+cd /comfyui-container/comfyui
+pip install --no-cache-dir torchvision==0.15.2+cu117 torchaudio==2.0.2+cu117 torch==2.0.1+cu117 xformers==0.0.20 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install -r requirements.txt
