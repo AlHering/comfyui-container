13baebde9edc46de471bf812ef0cdec534490053
Alexander Hering <dev.hering@gmail.com>
2023-09-16 15:37:59 +0200

    added runner script

---
+++ run_webui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#################################################
+# Access point for starting the webui
+#################################################
+
+source /comfyui-container/venv/bin/activate
+cd /comfyui-container/comfyui
+python main.py
