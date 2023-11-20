d610a4fb03ad06ad8969881bb39542aac71c561b
Alexander Hering <dev.hering@gmail.com>
2023-11-20 22:19:28 +0100

    not caching requirements

--- install_webui.sh
+++ install_webui.sh
@@ -3,4 +3,4 @@
 source /comfyui-container/venv/bin/activate
 cd /comfyui-container/comfyui
 pip install --no-cache-dir torchvision==0.15.2+cu117 torchaudio==2.0.2+cu117 torch==2.0.1+cu117 xformers==0.0.20 --extra-index-url https://download.pytorch.org/whl/cu117
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
