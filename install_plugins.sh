3c8a1e68015a16138500f137579121227f3133a6
Alexander Hering <dev.hering@gmail.com>
2024-01-21 20:38:24 +0100

    fixed installation path

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /comfyui-container/venv/bin/activate
-cd /mnt/Workspaces/Workspaces/docker/stable-diffusion/comfyui-container/comfyui/custom_nodes
+cd cd /comfyui-container/comfyui/custom_nodes
 
 plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git")
 for plugin in "${plugins[@]}"
