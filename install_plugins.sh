787aaf178e6396598f5fc63b5739ff2fd4e31fff
Alexander Hering <dev.hering@gmail.com>
2024-01-21 21:10:04 +0100

    fixed cd in plugin installation script

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 source /comfyui-container/venv/bin/activate
-cd cd /comfyui-container/comfyui/custom_nodes
+cd /comfyui-container/comfyui/custom_nodes
 
 plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git")
 for plugin in "${plugins[@]}"
