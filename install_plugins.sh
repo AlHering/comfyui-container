95d2e6a60bca836def576c4adf604dd542af1f29
Alexander Hering <dev.hering@gmail.com>
2024-01-22 22:32:20 +0100

    extended plugin list

--- install_plugins.sh
+++ install_plugins.sh
@@ -2,9 +2,10 @@
 source /comfyui-container/venv/bin/activate
 cd /comfyui-container/comfyui/custom_nodes
 
-plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git")
+plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git" "https://github.com/ltdrdata/ComfyUI-Impact-Pack.git" "https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git" "https://github.com/jags111/efficiency-nodes-comfyui.git" "https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git" "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git" "https://github.com/Fannovel16/comfyui_controlnet_aux.git" "https://github.com/cubiq/ComfyUI_IPAdapter_plus.git")
 for plugin in "${plugins[@]}"
 do
-    git clone $plugin
+    git clone $plugin --recursive
 done
 
+pip install insightface==0.7.3
