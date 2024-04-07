f5e15c90a841fb87e5b23631aecceee06805aaac
Alexander Hering <dev.hering@gmail.com>
2024-04-07 15:20:50 +0200

    adjusted prompt copying

--- copy_prompts.sh
+++ copy_prompts.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo Removing the following directories:
+find /comfyui-container/comfyui/custom_nodes/ComfyUI-Inspire-Pack/prompts/ -mindepth 1 -maxdepth 1 -type d \( ! -name example \) -print
+find /comfyui-container/comfyui/custom_nodes/ComfyUI-Inspire-Pack/prompts/ -mindepth 1 -maxdepth 1 -type d \( ! -name example \) -exec rm -rf "{}" \;
+echo Removing the following files:
+find /comfyui-container/comfyui/custom_nodes/ComfyUI-Inspire-Pack/prompts/ -type f \( ! -name put_prompts_dirs_here \) -print
+find /comfyui-container/comfyui/custom_nodes/ComfyUI-Inspire-Pack/prompts/ -type f \( ! -name put_prompts_dirs_here \) -delete
+
 cp -Ru /comfyui-container/stable_diffusion_prompts /comfyui-container/comfyui/custom_nodes/ComfyUI-Inspire-Pack/prompts/shared
