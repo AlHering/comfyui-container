7c216d3d9dd33b5980c3e5fe99e7f77ab2198575
Alexander Hering <dev.hering@gmail.com>
2024-04-21 13:01:07 +0200

    extended plugins

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,7 +3,7 @@ source /comfyui-container/venv/bin/activate
 cd /comfyui-container/comfyui/custom_nodes
 
 # Clone repos
-plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git" "https://github.com/ltdrdata/ComfyUI-Impact-Pack.git" "https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git" "https://github.com/jags111/efficiency-nodes-comfyui.git" "https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git" "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git" "https://github.com/Fannovel16/comfyui_controlnet_aux.git" "https://github.com/cubiq/ComfyUI_IPAdapter_plus.git" "https://github.com/TinyTerra/ComfyUI_tinyterraNodes.git" "https://github.com/WASasquatch/was-node-suite-comfyui.git" "https://github.com/palant/image-resize-comfyui")
+plugins=("https://github.com/ltdrdata/ComfyUI-Manager.git" "https://github.com/ltdrdata/ComfyUI-Impact-Pack.git" "https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git" "https://github.com/jags111/efficiency-nodes-comfyui.git" "https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git" "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git" "https://github.com/Fannovel16/comfyui_controlnet_aux.git" "https://github.com/cubiq/ComfyUI_IPAdapter_plus.git" "https://github.com/TinyTerra/ComfyUI_tinyterraNodes.git" "https://github.com/WASasquatch/was-node-suite-comfyui.git" "https://github.com/palant/image-resize-comfyui.git" "https://github.com/cubiq/ComfyUI_essentials.git" "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git")
 for plugin in "${plugins[@]}"
 do
     git clone $plugin --recursive
@@ -14,6 +14,7 @@ pip install insightface==0.7.3 simpleeval==0.9.13
 pip install segment-anything==1.0 omegaconf==2.3.0 
 pip install -r comfyui_controlnet_aux/requirements.txt
 pip install -r was-node-suite-comfyui/requirements.txt
+pip install -r ComfyUI_essentials/requirements.txt
 
 # Handle models
 mkdir /comfyui-container/comfyui/models/clip_vision
