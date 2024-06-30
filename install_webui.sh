f14e6690012d2ec2ffe19b38664247185468637a
Alexander Hering <dev.hering@gmail.com>
2024-06-30 18:54:02 +0200

    updated cuda version for torch installs to 12.1

--- install_webui.sh
+++ install_webui.sh
@@ -2,5 +2,5 @@
 
 source /comfyui-container/venv/bin/activate
 cd /comfyui-container/comfyui
-pip install --no-cache-dir torchvision==0.15.2+cu117 torchaudio==2.0.2+cu117 torch==2.0.1+cu117 xformers==0.0.20 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install --no-cache-dir torchvision==0.17.1+cu121 torchaudio==2.2.1+cu121 torch==2.2.1+cu121 xformers --extra-index-url https://download.pytorch.org/whl/cu121
 pip install --no-cache-dir -r requirements.txt
